e96d52847faf67c7a43714f456e688f9de0fc200
hhg.mzt <hiram.hdezg@gmail.com>
2024-06-20 12:10:42 -0700

    chore: se agregan las rutas de swagger

--- src/main/java/com/muebleria/inventarios/utils/Constants.java
+++ src/main/java/com/muebleria/inventarios/utils/Constants.java
@@ -7,5 +7,13 @@ public class Constants {
     public static final List<String>  UNPROTECTED_URS =  List.of(
             "/api/almacenes",
             "/api/usuarios",
-            "/api/generate-token");
+            "/api/generate-token",
+            "/swagger-ui.html",
+            "/swagger-ui/index.html",
+            "/openapi/openapi.yml",
+            "/swagger-ui/index.html",
+            "/swagger-ui/swagger-initializer.js",
+            "/swagger-ui/favicon-32x32.png",
+            "/v3/api-docs/swagger-config",
+            "/v3/api-docs");
 }
